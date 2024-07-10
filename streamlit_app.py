68a358277a52c7fbcbe777737d7ccfaba17ec0d5
luizferreira29 <62694183+luizferreira29@users.noreply.github.com>
2024-07-10 22:32:25 +0200

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import requests
+import json
+import pprint
+import requests
+from requests.auth import HTTPBasicAuth
+import pandas as pd
+import numpy as np
+
+from json2xml import json2xml
+
+from bs4 import BeautifulSoup
+from datetime import datetime
+from datetime import datetime
+
+st.title("Train checker for Vernouillet-Verneuil")
+st.caption("This app allows you to check if your train is on time.")
+
+apikey = st.text_input("apikey", type="default")
+
+def handle_train_check(url_param):
+    url = f"https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring?MonitoringRef={url_param}"
+    headers = {'Accept': 'application/json', 'apikey': apikey}
+    req = requests.get(url, headers=headers).json()
+    print('Status:',req)
+    response_xml = json2xml.Json2xml(req, wrapper="all", pretty=True).to_xml()
+    soup = BeautifulSoup(response_xml, features="xml")
+    destination = []
+    time_arrival = []
+    status = []
+    date_format = "%Y-%m-%dT%H:%M:%S.000Z"
+    for train in soup.find_all("MonitoredVehicleJourney"):
+        destination.append(train.find('DestinationName').text.strip())
+        time = train.find('ExpectedArrivalTime').text
+        date_obj = datetime.strptime(time, date_format)
+        date = str(date_obj.hour) + "h", str(date_obj.minute) + "min"
+        time_arrival.append(date)
+        status.append(train.find('DepartureStatus').text)
+    df = pd.DataFrame({"destination" : destination, "heure de passage" : time_arrival, "ok" : status})
+    st.table(df)
+
+if st.button("Vernouillet-Verneuil", type="primary"):
+    handle_train_check("STIF:StopPoint:Q:41198:")
+
+if st.button("Les clairières de Verneuil", type="primary"):
+    handle_train_check("STIF:StopPoint:Q:41199:")
